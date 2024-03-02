465a50c012eea6c2ee8f14f980605d3e62d95a35
Ashiqur Rahman <imashiqe@gmail.com>
2024-03-02 23:19:12 +0600

    java spring boot search

---
+++ src/main/java/com/google/search/MyGoogleApplication.java
@@ -0,0 +1,13 @@
+package com.google.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGoogleApplication.class, args);
+	}
+
+}
